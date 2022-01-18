553c48471bba58ec9d18002b3667eb1542a6a3e0
Patrick <p5150j@gmail.com>
2022-01-17 18:29:42 -0700

    first commit

---
+++ Assets/RestClient/DemoScene/Scripts/MainScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Models;
+using Proyecto26;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class MainScript : MonoBehaviour {
+
+	private readonly string basePath = "http://10.0.0.187:8888/relays/3";
+	private RequestHelper currentRequest;
+
+	private void LogMessage(string state, string message) {
+#if UNITY_EDITOR
+		EditorUtility.DisplayDialog (state, message, "Ok");
+#else
+		Debug.Log(message);
+#endif
+	}
+
+	
+
+	public void Post(){
+		RestClient.Post<ServerResponse>("http://10.0.0.187:8888/relays/3", new Post
+		{
+			state = true,
+		
+		}).Then(response => {
+			EditorUtility.DisplayDialog("ID: ", response.id, "Ok");
+			EditorUtility.DisplayDialog("Date: ", response.date, "Ok");
+		});
+	}
+
+	
+}
---
+++ Assets/RestClient/DemoScene/Scripts/Model/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Models
+{
+	[Serializable]
+	public class Post
+	{
+		
+
+		public bool state;
+
+
+
+		public override string ToString(){
+			return UnityEngine.JsonUtility.ToJson (this, true);
+		}
+	}
+
+
+
+	[Serializable]
+	public class ServerResponse
+	{
+		public string id;
+		public string date; //DateTime is not supported by JsonUtility
+	}
+}
+
